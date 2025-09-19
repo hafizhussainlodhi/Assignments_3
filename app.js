8da25a7774f7d7399175a4d32004222fb175becf
hafizhussainlodhi <hasanlodhi000@gmail.com>
2025-09-19 02:19:54 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,33 @@
+// ===========================
+// Chapter 3 - VARIABLES FOR NUMBERS
+// ===========================
+
+// Part 1
+// Declare a variable called age & assign to it your age. Show your age in an alert box.
+var age = 20;
+alert("My age is " + age);
+
+// ---------------------------
+
+// Part 2
+// Keep track of how many times a visitor has visited a web page.
+var visitCount = 5; // Example: assume visitor has visited 5 times
+document.write("You have visited this site " + visitCount + " times.<br>");
+
+// ---------------------------
+
+// Part 3
+// Declare a variable called birthYear & assign to it your birth year. Show it in browser.
+var birthYear = 2004;
+document.write("My birth year is " + birthYear + "<br>");
+
+// ---------------------------
+
+// Part 4
+// A visitor visits an online clothing store. Store visitor’s info and show message.
+var visitorName = "John Doe";
+var productTitle = "T-shirt(s)";
+var quantity = 5;
+
+document.write(visitorName + " ordered " + quantity + " " + productTitle + " on XYZ Clothing store.");
+
